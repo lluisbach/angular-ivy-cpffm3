efbc13af5a84ec3392ab5ee54b04dfcfd22f810b
lluisbach <lluisbachc@gmail.com>
2021-06-08 23:06:05 +0200

    6 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService, MessageService]
 })
 export class AppModule { }
